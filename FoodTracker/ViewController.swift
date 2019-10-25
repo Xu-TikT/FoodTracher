5c79a42e7da266c57eb2c66e5cdcf45a88ebec40
xwt <skysafari@foxmail.com>
2019-10-25 16:58:18 +0800

    练习图片选择

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 //MARK:Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+        
     }
+    
+    //MARK:UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        mealNameLabel.text = textField.text
+    }
+    //MARK:UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss this picker if the user canceled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
 //MARK:Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
@@ -24,6 +49,20 @@ class ViewController: UIViewController {
     @IBAction func test(_ sender: UIButton) {
         mealNameLabel.text = "test"
     }
+    //选择图片
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //当用户在输入文字时点击了图片，可以将输入框的焦点取消并隐藏
+        nameTextField.resignFirstResponder()
+        //create an image picker controller
+        //UIImagePickerController is a view controller that lets a user pick media form their photo library
+        let imagePickerController = UIImagePickerController()
+        //only allow photos to be picked,not taken
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        // Make sure ViewController is notified when the user picks an image.
+        present(imagePickerController,animated: true,completion: nil)
+    }
+    
     
 
 }
